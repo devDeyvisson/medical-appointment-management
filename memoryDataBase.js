4975c756cafbb50c18f0dad8d15e8c406fdc4acc
Deyvisson Menezes <deyvissonsantos3348@gmail.com>
2025-07-20 02:34:36 -0300

    criados arrays para simular um banco de dados em memória

---
+++ memoryDataBase.js
@@ -0,0 +1,9 @@
+let listDoctors = [];
+let listAllPatients = [];
+let listAppointment = [];
+
+module.exports = {
+  doctors: listDoctors,
+  patients: listAllPatients,
+  appointments: listAppointment,
+};
